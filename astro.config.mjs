f3273534fcad4e4ad9033252d35b125de455b957
Hayden <hayden@onelessthing.us>
2026-05-04 16:20:37 -0500

    Remove sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://onelessthing.us',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx()],
 });
